8fbf41d24cb9e07df7fa9966ec25adcb845a13f3
Elvin Yung <elvinyung1@gmail.com>
2014-11-29 23:32:52 -0500

    added books example

---
+++ examples/books-example/api.js
@@ -0,0 +1,22 @@
+var uberman = require('./../..');
+
+// grabs the key and cert from the current directory
+var booksAPI = uberman({
+	keyPath: process.cwd() + '/key.key',
+	certPath: process.cwd() + '/cert.crt'
+});
+
+// creates an endpoint for books
+booksAPI.addEndpoint('books', {
+  title: String,
+  genre: uberman.Types.ObjectId
+});
+
+// creates an endpoint for genres
+booksAPI.addEndpoint('genres', {
+	name: String,
+	books: [uberman.Types.ObjectId]
+});
+
+// the passphrase is `password`... try running the app!
+booksAPI.listen(8443);
---
+++ examples/quickstart-example/api.js
@@ -0,0 +1,21 @@
+var uberman = require('./../..');
+
+// grabs the key and cert from the current directory
+var blogAPI = uberman({
+	keyPath: process.cwd() + '/key.key',
+	certPath: process.cwd() + '/cert.crt'
+});
+
+// creates a basic CRUD endpoint
+blogAPI.addEndpoint('blogPosts', {
+  title: String,
+  body: String,
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  upvotes: Number
+});
+
+// the passphrase is `password`... try running the app!
+blogAPI.listen(8443);
