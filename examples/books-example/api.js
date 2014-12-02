fe80cb5699ee146210d7584a36b4796bfd3b21f9
Elvin Yung <elvinyung1@gmail.com>
2014-12-02 16:27:45 -0500

    updated books example to use foreign key type

--- examples/books-example/api.js
+++ examples/books-example/api.js
@@ -9,13 +9,13 @@ var booksAPI = uberman({
 // creates an endpoint for books
 booksAPI.addEndpoint('books', {
   title: String,
-  genre: uberman.Types.ObjectId
+  genre: uberman.Types.foreignKey('genres')
 });
 
 // creates an endpoint for genres
 booksAPI.addEndpoint('genres', {
 	name: String,
-	books: [uberman.Types.ObjectId]
+	books: [uberman.Types.foreignKey('books')]
 });
 
 // the passphrase is `password`... try running the app!
